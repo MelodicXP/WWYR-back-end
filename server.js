e810b82474e3bc2ad741aa5b4158a9461cea14ca
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-11-29 09:36:31 -0600

    Successfully connect to MongoDB with Mongoose

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Required dependencies - dotenv, express, cors, mongoose
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Assign mongoDB connection from .env to a varaible 'MONGODB_CONN'
+const MONGODB_CONN = process.env.MONGODB_CONN;
+
+// Add middleware - Assign express to variable 'app' and use with '.use'
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Assign port from .env to variable
+const PORT = process.env.PORT;
+
+// Connect to mongoose, and assign connection to variable 'db'
+mongoose.connect(MONGODB_CONN);
+const db = mongoose.connection;
+
+// Check if mongoose connection failure or sucess
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => console.log('Mongoose is connected'));
+
+// Check if port connection is successful
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
